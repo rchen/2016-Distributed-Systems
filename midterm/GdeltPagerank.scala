09a940d29b6bcf251e0e6bd50862626d56e184d9
yunayulee <105971021@nccu.edu.tw>
2016-11-08 21:28:52 +0800

    Update GdeltPagerank.scala

--- midterm/GdeltPagerank.scala
+++ midterm/GdeltPagerank.scala
@@ -8,5 +8,5 @@ val e = b.map(x => Edge(str2Long(x._1),str2Long(x._2),x._1+":"+x._2))
 val g = Graph(d,e)
 val ranks = g.pageRank(0.0001).vertices
 val ranksByUsername = d.join(ranks).map {case (id, (username, rank)) => (username, rank)}
-ranksByUsername.sortBy( x => x._2).foreach(println)
+ranksByUsername.sortBy( x => x._2).collect().foreach(println)
 System.exit(0)
